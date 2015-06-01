2d8f86da4010daf05cdd519ed04dc2974a4767a7
Pavel Baranchikov <p.a.baranchikov@gmail.com>
2015-06-01 12:17:21 +0300

    Prepared release 0.0.1

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 aclocal && autoheader && automake --add-missing -i && autoconf || exit 1
 
+./configure || exit 2
+
 make clean && make -j10 || exit 3
 
 
--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 /*
- * main.c
+ * Copyright 2015 Pavel Baranchikov
  *
- *  Created on: 28 мая 2015 г.
- *      Author: pavel
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <resolv.h>
@@ -52,7 +61,7 @@ NULL, /* connection cleanup */
 };
 
 const char** excludedDomains;
-const int loglevel = LOG_DEBUG;
+const int loglevel = LOG_INFO;
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -66,7 +75,7 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	syslog(LOG_INFO, "%s milter ssuccessfully started. Listening socket %s",
+	syslog(LOG_INFO, "%s milter successfully started. Listening socket %s",
 	PACKAGE, socketPath);
 	if ((excludedDomains = readExcludedDomains()))
 		return smfi_main();
